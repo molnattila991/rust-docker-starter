2f2a9908089c49c4b4aa87971701dc5824a0746c
molnarattila991 <molnarattila991@gmail.com>
2022-02-13 11:41:41 +0100

    (init) with cached dependencies in docker

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use mysql::prelude::Queryable;
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+use mysql::Opts;
+use mysql::Pool;
+use serde::Serialize;
+use std::env;
+
+#[derive(Serialize)]
+struct Product {
+    id: u64,
+    code: String,
+    price: f32,
+    product_name: String,
+}
+
+pub struct AppState {
+    pub pool: mysql::Pool,
+}
+
+#[get("/")]
+pub async fn say_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/jsonList")]
+pub async fn get_list(data: web::Data<AppState>) -> impl Responder {
+    let mut conn = data.pool.get_conn().unwrap();
+
+    let res = conn.query_map(
+        "select product_id, product_code, price, name from PRODUCT",
+        |(product_id, product_code, price, name)| 
+          Product {
+                id: product_id,
+                code: product_code,
+                price: price,
+                product_name: name
+            }
+    ).expect("Query failed.");
+     
+    web::Json(res)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mysql_connection_string = env::var("MYSQL_CONNECTION_STRING");
+    let opt = Opts::from_url(&mysql_connection_string.unwrap());
+    let pool = Pool::new(opt.unwrap()).unwrap();
+    let mut _conn = pool.get_conn().unwrap();
+
+    let app_data = web::Data::new(AppState { pool: pool });
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_data.clone())
+            .service(say_hello)
+    })
+    .workers(4)
+    .bind("0.0.0.0:12080")?
+    .run()
+    .await
+}
